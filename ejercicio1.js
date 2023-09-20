97c4a1bf231c1f1518a64a5fe19edddd8b14896b
Saul Paredes <saulkurosaki13@gmail.com>
2023-09-19 20:45:34 -0600

    ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+
+//Ejercicio1: Dado un Array de superheroes de Marvel, haz un programa que pueda
+            //mostrar la información de cada uno.
+            //Y añade la capacidad de buscar la información de varios superheroes a la vez.
+
+//Ejemplo: mostrarInformacionSuperheroe('Tony Stark')
