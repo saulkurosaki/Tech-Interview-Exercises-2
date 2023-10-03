a41439f813fee257454b860943d38e089c016b83
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-03 12:33:25 -0600

    ejercicio20 Refactor

--- ejercicio20.js
+++ ejercicio20.js
@@ -1,19 +1,6 @@
 
 //Ejercicio20: Crea una función que ordene un array de objetos, en base a las propiedades que le pase por parametro.
 
-// const usuarios = [
-//     { nombre: 'Antonio', edad: 20 },
-//     { nombre: 'Juan', edad: 23 },
-//     { nombre: 'Pepe', edad: 12 },
-//     { nombre: 'Raul', edad: 28 },
-//     { nombre: 'Paco', edad: 38 },
-//     { nombre: 'Jason', edad: 56}
-//   ];
-
-
-// Ejemplos:
-// ordenarObjetos(usuarios, "edad");
-
 const ordenarObjetos = (datos, propiedad) => {
 
     return datos.sort((a, b) => {
