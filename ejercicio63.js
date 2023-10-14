202d75687da9fee651d766003e6e3e49ae1c5b22
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-14 12:27:37 -0600

    ejercicio63 Complete

---
+++ ejercicio63.js
@@ -0,0 +1,22 @@
+
+//Ejercicio63: Crea una funcion para conseguir las sumas acumuladas de un array de numeros.
+
+// Ejemplos:
+// sumasAcumuladas([1, 2, 3, 4])   // [1, 3, 6, 10] 
+ 
+// (se calcula como: [1, 1+2, 1+2+3, 1+2+3+4])
+
+const sumasAcumuladas = (numeros) => {
+
+    let resultado = [];
+    let suma = 0;
+
+    for(let numero of numeros){
+
+        suma += numero;
+
+    };
+
+};
+
+console.log(sumasAcumuladas([1, 2, 3, 4]));
